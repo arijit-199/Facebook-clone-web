28fa74e5ed2be25ea7f67fdb90f74e56659b0b2d
Arijit Ghosh <117435897+arijit-199@users.noreply.github.com>
2023-12-26 18:24:36 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationships.js";
+import multer from "multer";
+
+
+const app = express();
+
+
+// MIDDLEWARES
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:3000"
+    }));
+app.use(cookieParser());
+
+
+// UPLOAD FILES
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../frontend/public/upload');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage })
+    
+app.post("/api/upload/files", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+})
+
+
+// API
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/relationships", relationshipRoutes);
+
+// PORT
+app.listen(8800, () => {
+    console.log("API working!");
+});
