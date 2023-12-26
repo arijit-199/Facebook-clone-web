a7102318506f1e803fbcd0ba18af6b4680c990be
Arijit Ghosh <117435897+arijit-199@users.noreply.github.com>
2023-12-26 20:03:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -53,10 +53,6 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/relationships", relationshipRoutes);
 
-app.use("/", function() {
-    console.log("Hi, I m here..");
-});
-
 // PORT
 app.listen(8800, () => {
     console.log("API working!");
