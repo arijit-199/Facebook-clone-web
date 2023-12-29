40fd9d6502e3a49323cbfbe5f70c21578954e1fd
Arijit Ghosh <117435897+arijit-199@users.noreply.github.com>
2023-12-29 14:29:30 +0530

    Update final index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://heroic-medovik-8f9d07.netlify.app"
+        origin: "https://658e89803130558a8ca631eb--fantastic-fairy-7e5e62.netlify.ap"
     }));
 app.use(cookieParser());
 
